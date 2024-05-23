13faad320fdd1cfa22f84cd41904327e1c561316
sylamsh <srisylamsh.amrutakavi@amagi.com>
2024-05-23 14:37:27 +0530

    sync with go-commons logger v2

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -2,12 +2,23 @@ package logger
 
 import (
 	"encoding/json"
+	"sync"
 
+	"github.com/nvg14/logit/pkg/utils"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-func NewCommonLogger(moduleName, logLevel, productID, environment, application string, initialFields map[string]interface{}) *zap.SugaredLogger {
+var once sync.Once
+
+func NewSugaredLogger(logLevel, productID, environment, application string, initialFields map[string]interface{}) *zap.SugaredLogger {
+	initialFields[LGR_KEY_PRODUCT_ID] = productID
+	initialFields[LGR_KEY_ENVIRONMENT] = environment
+	initialFields[LGR_KEY_APPLICATION_ID] = application
+	return NewZapLogger(logLevel, initialFields).Sugar()
+}
+
+func NewZapLogger(logLevel string, initialFields map[string]interface{}) *zap.Logger {
 	var rawJSON = []byte(`{
 		"level": "` + logLevel + `",
 		"encoding": "json",
@@ -20,45 +31,38 @@ func NewCommonLogger(moduleName, logLevel, productID, environment, application s
 		}
 	}`)
 
-	config := setUpConfig(moduleName, rawJSON, initialFields)
+	config := setUpConfig(rawJSON, initialFields)
 	config.OutputPaths = []string{"stdout"}
 	config.ErrorOutputPaths = []string{"stdout"}
-	logit, _ := config.Build()
+	zapLogger, _ := config.Build()
 
-	return logit.Sugar()
+	return zapLogger
 }
 
-func NewFastLogger(moduleName, logLevel string, initialFields map[string]interface{}) *zap.Logger {
-	var rawJSON = []byte(`{
-		"level": "` + logLevel + `",
-		"encoding": "json",
-		"encoderConfig": {
-			"levelKey": "level",
-			"messageKey": "message",
-			"levelEncoder": "lowercase",
-			"nameKey":"name",
-			"stacktraceKey":"stack"
-		}
-	}`)
-
-	config := setUpConfig(moduleName, rawJSON, initialFields)
-	config.OutputPaths = []string{"stdout"}
-	config.ErrorOutputPaths = []string{"stdout"}
-	logit, _ := config.Build()
-
-	return logit
-}
-
-func setUpConfig(moduleName string, rawJSON []byte, initialFields map[string]interface{}) (config zap.Config) {
+func setUpConfig(rawJSON []byte, initialFields map[string]interface{}) (config zap.Config) {
 	_ = json.Unmarshal(rawJSON, &config)
 	config.EncoderConfig.TimeKey = "timestamp"
 	config.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout("2006-01-02T15:04:05Z")
 	if config.InitialFields == nil {
 		config.InitialFields = make(map[string]interface{})
 	}
-	config.InitialFields["module"] = moduleName
 	for k, v := range initialFields {
 		config.InitialFields[k] = v
 	}
 	return config
+}
+
+func init() {
+	once.Do(func() {
+		zapLogger := NewZapLogger(
+			utils.GetEnvWithDefault("LOG_LEVEL", "info"),
+			map[string]interface{}{
+				LGR_KEY_PRODUCT_ID:     utils.GetEnvWithDefault("PRODUCT_ID", "amaginow"),
+				LGR_KEY_ENVIRONMENT:    utils.GetEnvWithDefault("ENVIRONMENT", "development"),
+				LGR_KEY_APPLICATION_ID: utils.GetEnvWithDefault("APPLICATION_ID", "master"),
+			},
+		)
+		defer zapLogger.Sync()
+		intializeLogAdapter(zapLogger.Sugar())
+	})
 }
--- pkg/logger/wrapper.go
+++ pkg/logger/wrapper.go
@@ -1,79 +1,176 @@
 package logger
 
 import (
+	"context"
+	"errors"
+
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 )
 
-type CommonLogitAdapter struct {
+type CommonLogAdapter struct {
 	commonLogger *zap.SugaredLogger
-	productID    string
-	environment  string
-	application  string
 }
 
-// NewLogitAdapter ... which uses general log interface
-func NewCommonLogitAdapter(moduleName, logLevel, productID, environment, application string, initialFields map[string]interface{}) *CommonLogitAdapter {
-	zapLogger := NewCommonLogger(moduleName, logLevel, productID, environment, application, initialFields)
-	return &CommonLogitAdapter{
-		commonLogger: zapLogger.WithOptions(zap.AddCallerSkip(1), zap.AddStacktrace(zap.ErrorLevel)),
-		productID:    productID,
-		environment:  environment,
-		application:  application,
+var commonLogAdapter CommonLogAdapter
+
+func intializeLogAdapter(logger *zap.SugaredLogger) {
+	commonLogAdapter = CommonLogAdapter{
+		commonLogger: logger.WithOptions(zap.AddCallerSkip(1), zap.AddStacktrace(zap.ErrorLevel)),
 	}
 }
 
-func addKeyVals(productID, environment, application, activity, event string, otherKeyVals ...any) []any {
+func GetCommonLogger() *CommonLogAdapter {
+	return &commonLogAdapter
+}
+
+func addKeyVals(activity LogActivity, event LogEvent, otherKeyVals ...any) []any {
 	var keyVals []any
+
 	keyVals = append(keyVals,
-		"activity", activity,
-		"event", event,
-		"product", productID,
-		"environment", environment,
-		"application", application,
+		LGR_KEY_ACTIVITY, string(activity),
+		LGR_KEY_EVENT, string(event),
 	)
 	keyVals = append(keyVals, otherKeyVals...)
 
 	return keyVals
 }
 
-func (log *CommonLogitAdapter) Debug(msg string, keyvals ...any) {
-	keyvals = addKeyVals(log.productID, log.environment, log.application, "activity", "event", keyvals...)
-	log.commonLogger.Debugf(msg, keyvals...)
+func (adapter *CommonLogAdapter) Debug(msg string, keyvals ...any) {
+	adapter.commonLogger.Debugf(msg, keyvals...)
 }
 
-func (log *CommonLogitAdapter) Info(msg string, keyvals ...any) {
-	log.commonLogger.Infof(msg, keyvals...)
+func (adapter *CommonLogAdapter) Info(msg string, keyvals ...any) {
+	adapter.commonLogger.Infof(msg, keyvals...)
 }
 
-func (log *CommonLogitAdapter) Warn(msg string, keyvals ...any) {
-	log.commonLogger.Warnf(msg, keyvals...)
+func (adapter *CommonLogAdapter) Warn(msg string, keyvals ...any) {
+	adapter.commonLogger.Warnf(msg, keyvals...)
 }
 
-func (log *CommonLogitAdapter) Error(msg string, keyvals ...any) {
-	log.commonLogger.Errorf(msg, keyvals...)
+func (adapter *CommonLogAdapter) Error(msg string, keyvals ...any) {
+	adapter.commonLogger.Errorf(msg, keyvals...)
 }
 
-func (log *CommonLogitAdapter) Debugw(message string, activity string, event string, otherKeyVals ...any) {
-	keyVals := addKeyVals(log.productID, log.environment, log.application, activity, event, otherKeyVals...)
-	log.commonLogger.Debugw(message, keyVals...)
+func (adapter *CommonLogAdapter) Debugw(message string, activity LogActivity, event LogEvent, otherKeyVals ...any) {
+	keyVals := addKeyVals(activity, event, otherKeyVals...)
+	adapter.commonLogger.Debugw(message, keyVals...)
 }
 
-func (log *CommonLogitAdapter) Infow(message string, activity string, event string, otherKeyVals ...any) {
-	keyVals := addKeyVals(log.productID, log.environment, log.application, activity, event, otherKeyVals...)
-	log.commonLogger.Infow(message, keyVals...)
+func (adapter *CommonLogAdapter) Infow(message string, activity LogActivity, event LogEvent, otherKeyVals ...any) {
+	keyVals := addKeyVals(activity, event, otherKeyVals...)
+	adapter.commonLogger.Infow(message, keyVals...)
 }
 
-func (log *CommonLogitAdapter) Warnw(message string, activity string, event string, otherKeyVals ...any) {
-	keyVals := addKeyVals(log.productID, log.environment, log.application, activity, event, otherKeyVals...)
-	log.commonLogger.Warnw(message, keyVals...)
+func (adapter *CommonLogAdapter) Warnw(message string, activity LogActivity, event LogEvent, otherKeyVals ...any) {
+	keyVals := addKeyVals(activity, event, otherKeyVals...)
+	adapter.commonLogger.Warnw(message, keyVals...)
 }
 
-func (log *CommonLogitAdapter) Errorw(message string, activity string, event string, otherKeyVals ...any) {
-	keyVals := addKeyVals(log.productID, log.environment, log.application, activity, event, otherKeyVals...)
-	log.commonLogger.Errorw(message, keyVals...)
+func (adapter *CommonLogAdapter) Errorw(message string, activity LogActivity, event LogEvent, otherKeyVals ...any) {
+	keyVals := addKeyVals(activity, event, otherKeyVals...)
+	adapter.commonLogger.Errorw(message, keyVals...)
 }
 
-func (log *CommonLogitAdapter) Fatalw(message string, activity string, event string, otherKeyVals ...any) {
-	keyVals := addKeyVals(log.productID, log.environment, log.application, activity, event, otherKeyVals...)
-	log.commonLogger.Fatalw(message, keyVals...)
+func (adapter *CommonLogAdapter) Fatalw(message string, activity LogActivity, event LogEvent, otherKeyVals ...any) {
+	keyVals := addKeyVals(activity, event, otherKeyVals...)
+	adapter.commonLogger.Fatalw(message, keyVals...)
+}
+
+func Debug(message string, activity LogActivity, event LogEvent, otherKeyVals ...any) {
+	keyVals := addKeyVals(activity, event, otherKeyVals...)
+	commonLogAdapter.commonLogger.Debugw(message, keyVals...)
+}
+
+func Info(message string, activity LogActivity, event LogEvent, otherKeyVals ...any) {
+	keyVals := addKeyVals(activity, event, otherKeyVals...)
+	commonLogAdapter.commonLogger.Infow(message, keyVals...)
+}
+
+func Warn(message string, activity LogActivity, event LogEvent, otherKeyVals ...any) {
+	keyVals := addKeyVals(activity, event, otherKeyVals...)
+	commonLogAdapter.commonLogger.Warnw(message, keyVals...)
+}
+
+func Error(message string, activity LogActivity, event LogEvent, otherKeyVals ...any) {
+	keyVals := addKeyVals(activity, event, otherKeyVals...)
+	commonLogAdapter.commonLogger.Errorw(message, keyVals...)
+}
+
+func Fatal(message string, activity LogActivity, event LogEvent, otherKeyVals ...any) {
+	keyVals := addKeyVals(activity, event, otherKeyVals...)
+	commonLogAdapter.commonLogger.Fatalw(message, keyVals...)
+}
+
+// context containing
+//    span created by otel
+//    activity set with key CTX_KEY_ACTIVITY(required)
+//    amg_id set with key CTX_KEY_AMGID
+//    channel_id set with key CTX_KEY_CHANNELID
+
+func DebugCtx(ctx context.Context, message string, event LogEvent, otherKeyVals ...any) {
+	otherKeyVals = append(otherKeyVals, getValuesFromContext(ctx)...)
+	keyVals := addKeyVals(getActivity(ctx), event, otherKeyVals...)
+	commonLogAdapter.commonLogger.Debugw(message, keyVals...)
+}
+
+func InfoCtx(ctx context.Context, message string, event LogEvent, otherKeyVals ...any) {
+	otherKeyVals = append(otherKeyVals, getValuesFromContext(ctx)...)
+	keyVals := addKeyVals(getActivity(ctx), event, otherKeyVals...)
+	commonLogAdapter.commonLogger.Infow(message, keyVals...)
+}
+
+func WarnCtx(ctx context.Context, message string, event LogEvent, otherKeyVals ...any) {
+	otherKeyVals = append(otherKeyVals, getValuesFromContext(ctx)...)
+	keyVals := addKeyVals(getActivity(ctx), event, otherKeyVals...)
+	commonLogAdapter.commonLogger.Warnw(message, keyVals...)
+}
+
+func ErrorCtx(ctx context.Context, message string, event LogEvent, otherKeyVals ...any) {
+	otherKeyVals = append(otherKeyVals, getValuesFromContext(ctx)...)
+	keyVals := addKeyVals(getActivity(ctx), event, otherKeyVals...)
+	commonLogAdapter.commonLogger.Errorw(message, keyVals...)
+}
+
+func FatalCtx(ctx context.Context, message string, event LogEvent, otherKeyVals ...any) {
+	otherKeyVals = append(otherKeyVals, getValuesFromContext(ctx)...)
+	keyVals := addKeyVals(getActivity(ctx), event, otherKeyVals...)
+	commonLogAdapter.commonLogger.Fatalw(message, keyVals...)
+}
+
+func getActivity(ctx context.Context) LogActivity {
+	activity, ok := ctx.Value(CTX_KEY_ACTIVITY).(LogActivity)
+	if !ok {
+		err := errors.New("please set activity of type logger.LogActivity for logging in ctx")
+		panic(err)
+	}
+	return LogActivity(activity)
+}
+
+func getValuesFromContext(ctx context.Context) []any {
+	var keyVals []any
+	amgid, ok := ctx.Value(CTX_KEY_AMGID).(string)
+	if ok {
+		keyVals = append(keyVals, LGR_KEY_AMGID, amgid)
+	}
+	channelid, ok := ctx.Value(CTX_KEY_CHANNELID).(string)
+	if ok {
+		keyVals = append(keyVals, LGR_KEY_CHANNELID, channelid)
+	}
+	traceContext := getTraceContext(ctx)
+	if traceContext != nil {
+		keyVals = append(keyVals, LGR_KEY_TRACECONTEXT, traceContext)
+	}
+	return keyVals
+}
+
+func getTraceContext(ctx context.Context) map[string]string {
+	spanCtx := trace.SpanContextFromContext(ctx)
+	if !spanCtx.HasTraceID() || !spanCtx.HasSpanID() {
+		return nil
+	}
+	return map[string]string{
+		LGR_KEY_TRACEPARENT: "00-" + spanCtx.TraceID().String() + "-" + spanCtx.SpanID().String() + "-" + spanCtx.TraceFlags().String(),
+		LGR_KEY_TRACESTATE:  spanCtx.TraceState().String(),
+	}
 }
